7eb9724730ae15b041de71c4f5c0cb9add3267f7
Hazem Hasan <hasanh@PO-GSCOP-680.grenoble-inp.lan>
2023-09-12 13:45:37 +0200

    database

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_wtf import Form
 
+db = SQLAlchemy()
 app = Flask(__name__)
 
+# Create the database instance and connect the app file to database.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db.init_app(app)
+app.config['SECRET_KEY'] = 'thisisasecretkeylkjsfdpzoeruzperuyp'
+
+
+# *************************************
+
+# La class user pour la base de données
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+
+
+# *************************************
 
 @app.route('/')
 def home():
@@ -16,6 +36,11 @@ def dashboard():
 @app.route('/admin')
 def admin():
     return render_template('admin.html')
+
+
+@app.route('/logout')
+def logout():
+    return render_template('logout.html')
 
 
 if __name__ == '__main__':
